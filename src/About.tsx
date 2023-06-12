31292ca37ac09bebe4799c84aede06337b3a42b5
Fernando Alvarez Hayes <fernandoalvarez1990@gmail.com>
2023-06-11 22:55:40 -0300

    agregamos primera version

---
+++ src/About.tsx
@@ -0,0 +1,13 @@
+import "./Home.css";
+
+function About() {
+  return (
+    <div className="App">
+      <h1>About page</h1>
+      <p className="read-the-docs">Welcome to the About page!</p>
+      <p>Programación en español</p>
+    </div>
+  );
+}
+
+export default About;
