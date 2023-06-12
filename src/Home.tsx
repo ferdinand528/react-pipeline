48cc2ef94fd3c275661b4b0d7b96e355a318be83
Fernando Alvarez Hayes <fernandoalvarez1990@gmail.com>
2023-06-11 23:24:48 -0300

    agregamos primera version

--- src/Home.tsx
+++ src/Home.tsx
@@ -15,7 +15,7 @@ function Home() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Una pagina del ferd</h1>
       <div className="card">
         <button onClick={() => setCount((count: number) => count + 1)}>
           count is {count}
