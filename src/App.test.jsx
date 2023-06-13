379ca3f33d27d56ac9603dae42fabf13422f8a66
Fernando Alvarez Hayes <fernandoalvarez1990@gmail.com>
2023-06-13 19:44:04 -0300

    add test 2do Parcial de Ing de Sw - 2023

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Verificar texto del encabezado', () => {
+test('Test Titulo', () => {
   render(<App />);
   const headerText = screen.getByText(/2do Parcial de Ing de Sw - 2023/i);
   expect(headerText).toBeInTheDocument();
