085c3786187423eb5c7d3d8616b66aca6dee7dee
Fernando Alvarez Hayes <fernandoalvarez1990@gmail.com>
2023-06-15 13:26:59 -0300

    add 202 v 1.0.1

--- src/Home.tsx
+++ src/Home.tsx
@@ -21,7 +21,7 @@ function Home() {
           count is {count}
         </button>
         <p>
-          <code>Fernandez Alvarez Hayes</code> Fernando David - V1.0.0
+          <code>Fernandez Alvarez Hayes</code> Fernando David - V1.0.1
         </p>
       </div>
       <p className="read-the-docs">
